702d3ff93604eaa93dcfa8591bfddaf4faa70b2c
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-09-15 14:18:41 -0300

    adjustment

--- src/main/kotlin/com/mercadolivro/service/CustomerService.kt
+++ src/main/kotlin/com/mercadolivro/service/CustomerService.kt
@@ -1,7 +1,5 @@
 package com.mercadolivro.service
 
-import com.mercadolivro.controller.request.PostCustomerRequest
-import com.mercadolivro.controller.request.PutCustomerRequest
 import com.mercadolivro.model.CustomerModel
 import org.springframework.stereotype.Service
 
@@ -19,15 +17,17 @@ class CustomerService {
         return customers
     }
 
-    fun create(customer: PostCustomerRequest) {
+    fun create(customer: CustomerModel) {
 
         val id = if (customers.isEmpty()) {
             1
         } else {
-            customers.last().id.toInt() + 1
+            customers.last().id!!.toInt() + 1
         }.toString()
 
-        customers.add(CustomerModel(id, customer.name, customer.email))
+        customer.id = id
+
+        customers.add(customer)
     }
 
     fun getCustomer(id: String): CustomerModel {
@@ -35,9 +35,9 @@ class CustomerService {
         return customers.first { it.id == id }
     }
 
-    fun update(id: String, customer: PutCustomerRequest) {
+    fun update(customer: CustomerModel) {
 
-        customers.first { it.id == id }.let {
+        customers.first { it.id == customer.id }.let {
             it.name = customer.name
             it.email = customer.email
         }
