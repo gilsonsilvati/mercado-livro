6b1132c2960523e5d863357e752cda843128a73b
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-09-15 19:13:56 -0300

    Implemented customer repository

---
+++ src/main/kotlin/com/mercadolivro/repository/CustomerRepository.kt
@@ -0,0 +1,10 @@
+package com.mercadolivro.repository
+
+import com.mercadolivro.model.CustomerModel
+import org.springframework.data.repository.CrudRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface CustomerRepository : CrudRepository<CustomerModel, Int> {
+    fun findByNameContaining(name: String): List<CustomerModel>
+}
