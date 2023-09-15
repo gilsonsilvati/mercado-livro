e4693f99664980d3d588d202df5bbf57258d6795
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-09-15 17:05:26 -0300

    Transforming Customer Model into an entity and adjusted type id

--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,3 +1,16 @@
 package com.mercadolivro.model
 
-data class CustomerModel(var id: String? = null, var name: String, var email: String)
+import jakarta.persistence.Entity
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+
+@Entity(name = "customer")
+data class CustomerModel(
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    var id: Int? = null,
+    var name: String,
+    var email: String
+)
