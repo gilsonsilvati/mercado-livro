4dd37a01e63b20626c2e3d02c203139c0153edcf
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-10-04 10:59:45 -0300

    Adding env file

--- src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
+++ src/main/kotlin/com/mercadolivro/MercadoLivroApplication.kt
@@ -2,10 +2,12 @@ package com.mercadolivro
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.context.annotation.PropertySource
 import org.springframework.scheduling.annotation.EnableAsync
 
 @EnableAsync
 @SpringBootApplication
+@PropertySource(value = ["file:.env"], ignoreResourceNotFound = true)
 class MercadoLivroApplication
 
 fun main(args: Array<String>) {
