7ea1cdd7230729774035a1e36d2affccad94d0ec
Gilson Silva <ext.gilsoncs@unicid.edu.br>
2023-10-04 11:46:20 -0300

    Creating role table

--- src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
+++ src/main/kotlin/com/mercadolivro/model/CustomerModel.kt
@@ -1,12 +1,18 @@
 package com.mercadolivro.model
 
 import com.mercadolivro.enums.CustomerStatus
+import com.mercadolivro.enums.Profile
+import jakarta.persistence.CollectionTable
+import jakarta.persistence.Column
+import jakarta.persistence.ElementCollection
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
 import jakarta.persistence.Enumerated
+import jakarta.persistence.FetchType
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
 
 @Entity(name = "customer")
 data class CustomerModel(
@@ -17,6 +23,12 @@ data class CustomerModel(
 
     @Enumerated(EnumType.STRING)
     var status: CustomerStatus,
+
+    @Column(name = "role")
+    @Enumerated(EnumType.STRING)
+    @ElementCollection(targetClass = Profile::class, fetch = FetchType.EAGER)
+    @CollectionTable(name = "customer_role", joinColumns = [JoinColumn(name = "customer_id")])
+    var roles: Set<Profile> = setOf(),
 
     var name: String,
     var email: String,
